9728ffad9a269622b7ae0d225ff2702daa422721
Joseph <joseph264@verizon.net>
2016-10-11 21:54:52 -0400

    Updated file documentation.
     Changes to be committed:
    	modified:   I2S.cpp

--- libraries/I2S/I2S.cpp
+++ libraries/I2S/I2S.cpp
@@ -1,17 +1,27 @@
-/*
-
- I2S - Summarize your library here.
-
- Describe your library here.
-
- License: GNU General Public License V3
-
- (C) Copyright (Your Name Here)
-
- For more help on how to make an Arduino style library:
- http://arduino.cc/en/Hacking/LibraryTutorial
-
- */
+---------------------------------------------------------------------
+----                                                             ----
+---- Copyright (C) 2016 Joseph A. Consugar                       ----                       ----
+----                                                             ----
+---- This source file may be used and distributed without        ----
+---- restriction provided that this copyright statement is not   ----
+---- removed from the file and that any derivative work contains ----
+---- the original copyright notice and the associated disclaimer.----
+----                                                             ----
+----     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ----
+---- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ----
+---- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ----
+---- FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ----
+---- OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ----
+---- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ----
+---- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ----
+---- GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ----
+---- BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ----
+---- LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ----
+---- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ----
+---- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ----
+---- POSSIBILITY OF SUCH DAMAGE.                                 ----
+----                                                             ----
+---------------------------------------------------------------------
 
 #include "Arduino.h"
 #include "I2S.h"
@@ -21,21 +31,35 @@
 #define I2S_DATA      REGISTER(IO_SLOT(wishboneSlot), 0)
 #define I2S_SAMPLE    REGISTER(IO_SLOT(wishboneSlot), 1)
 
+/* Constructor.
+ */
 I2S::I2S()
 {
 
 }
 
+/* Initialization routine.
+ * Used to set the wishbone slot being used.
+ */
 void I2S::setup(unsigned int wishboneSlot)
 {
     this->wishboneSlot = wishboneSlot;
 }
 
+/* Read the most recent sample from the I2S interface.
+ * Sample is 32 bits where the upper 16 bits are
+ * the left channel, the lower 16 bits are the
+ * right channel.  The 16 bit sample is taken to
+ * be twos-complement.
+ */
 unsigned long I2S::readSample()
 {
     return I2S_SAMPLE;
 }
 
+/* These are test routines.  The data written to/
+ * read from the block doesn't do anything.
+ */
 unsigned long I2S::readData()
 {
     return I2S_DATA;

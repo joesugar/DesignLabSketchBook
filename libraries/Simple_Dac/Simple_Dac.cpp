44ec038de659125f4f16b145c501a3ab51c00f2d
Joseph <joseph264@verizon.net>
2016-10-11 21:54:24 -0400

    Updated file documentation.
     Changes to be committed:
    	modified:   Simple_Dac.cpp
    	modified:   Simple_Dac.vhd

--- libraries/Simple_Dac/Simple_Dac.cpp
+++ libraries/Simple_Dac/Simple_Dac.cpp
@@ -1,38 +1,65 @@
-/*
-
- Simple_Dac - Summarize your library here.
-
- Describe your library here.
-
- License: GNU General Public License V3
-
- (C) Copyright (Your Name Here)
-
- For more help on how to make an Arduino style library:
- http://arduino.cc/en/Hacking/LibraryTutorial
-
- */
+---------------------------------------------------------------------
+----                                                             ----
+---- Copyright (C) 2016 Joseph A. Consugar                       ----                       ----
+----                                                             ----
+---- This source file may be used and distributed without        ----
+---- restriction provided that this copyright statement is not   ----
+---- removed from the file and that any derivative work contains ----
+---- the original copyright notice and the associated disclaimer.----
+----                                                             ----
+----     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ----
+---- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ----
+---- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ----
+---- FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ----
+---- OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ----
+---- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ----
+---- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ----
+---- GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ----
+---- BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ----
+---- LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ----
+---- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ----
+---- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ----
+---- POSSIBILITY OF SUCH DAMAGE.                                 ----
+----                                                             ----
+---------------------------------------------------------------------
 
 #include "Arduino.h"
 #include "Simple_Dac.h"
 
 #define AUDIO_SAMPLE    REGISTER(IO_SLOT(wishboneSlot),0)
 
+/* Constructor.
+ */
 Simple_Dac::Simple_Dac()
 {
 
 }
 
+/* Initialization routine.
+ * Used to set the wishbone slot being used.
+ */
 void Simple_Dac::setup(unsigned int wishboneSlot)
 {
 	this->wishboneSlot = wishboneSlot;
 }
 
+/* Read the sample back from the DAC.
+ * Sample is 32 bits where the upper 16 bits are
+ * the left channel, the lower 16 bits are the
+ * right channel.  The 16 bit sample is taken to
+ * be twos-complement.
+ */
 unsigned long Simple_Dac::readSample()
 {
 	return AUDIO_SAMPLE;
 }
 
+/* Write the sample to the DAC.
+ * Sample is 32 bits where the upper 16 bits are
+ * the left channel, the lower 16 bits are the
+ * right channel.  The 16 bit sample is taken to
+ * be twos-complement.
+ */
 void Simple_Dac::writeSample(unsigned long value)
 {
 	AUDIO_SAMPLE = value;
--- libraries/Simple_Dac/Simple_Dac.vhd
+++ libraries/Simple_Dac/Simple_Dac.vhd
@@ -26,6 +26,10 @@
 -- This file is a Wishbone interface for the simple DAC included
 -- as part of the Papilio Design Lab software.
 --
+-- There are two channels.  The input is a 32-bit sample pair
+-- where the upper 16 bits are the left channel, the lower 16
+-- bits are the right channel.
+--
 
 library ieee;
 use ieee.std_logic_1164.ALL;
